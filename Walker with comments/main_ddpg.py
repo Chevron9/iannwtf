c2b5fc5cbea14a06c45657cc145eaf1a0cf3de79
chevron9 <ganymed.daniel@gmail.com>
2021-03-23 16:53:54 +0100

    make checkpoint loading suitable for continuing training

--- Walker with comments/main_ddpg.py
+++ Walker with comments/main_ddpg.py
@@ -7,25 +7,27 @@ import time
 from agent import Agent
 from plot import plot_learning_curve
 
-#Housekeeping variables
-last_score = last_avg_score = 0
-avg_delta = []
-
-
-
-
 
 if __name__ == '__main__':
+    #for the case you just want to load a previous model
+    load_checkpoint = False
+
+    #Housekeeping variables
+    last_score = last_avg_score = 0
+    avg_delta = avg_steps = []
+
     t = t_start = time.localtime()
     current_time = time.strftime("%Y-%m-%d-%H:%M:%S", t)
-    print(f"\n----------------- Training started at {current_time}. -------------------\n")
+    print(f"\n----------------- Training started at {current_time}. -------------------\ncheckpoint: {load_checkpoint}")
 
     #initialize the environment for the agent and initialize the agent
     #tf.debugging.set_log_device_placement(True)
     env = gym.make('BipedalWalker-v3')
+    #env = gym.make('BipedalWalkerHardcore-v3')
     noise = 0.4
+    # NEW batch 128
     agent = Agent(alpha=0.00005, beta=0.0005, input_dims=[24], tau=0.001, env=env,
-                  batch_size=64, dense1=512, dense2=512, n_actions=4, noise = noise)
+                  batch_size=128, dense1=512, dense2=512, n_actions=4, noise = noise)
 
 
     episodes = 250 #250
@@ -37,8 +39,6 @@ if __name__ == '__main__':
     best_score = env.reward_range[0]
     score_history = []
 
-    #for the case you just want to load a previous model
-    load_checkpoint = False
 
     #initializes the model with one random sample batch if model are loaded
     if load_checkpoint:
@@ -51,9 +51,6 @@ if __name__ == '__main__':
             n_steps += 1
         agent.learn()
         agent.load_models()
-        evaluate = True
-    else:
-        evaluate = False
 
     #main learning loop
     try:
@@ -74,7 +71,7 @@ if __name__ == '__main__':
             #on them
             steps = 0
             while not done:
-                action = agent.choose_action(observation, evaluate)
+                action = agent.choose_action(observation)
 
                 observation_, reward, done, info = env.step(action)
                 steps += 1
@@ -94,10 +91,11 @@ if __name__ == '__main__':
                 best_score = avg_score
                 if not load_checkpoint:
                     agent.save_models()
+                    last_save = current_episode
 
-            #calculating episode time etc.
+            #calculating and giving some info on training progress
             t_new = time.localtime()
-            current_time = time.strftime("%H:%M:%S", t)
+            current_time = time.strftime("%H:%M:%S", t_new)
             t_delta = time.mktime(t_new)-time.mktime(t)
             t = t_new
             avg_delta.append(t_delta)
@@ -108,14 +106,17 @@ if __name__ == '__main__':
             ETA_min = (episodes-i)*max((avg_delta_mean-avg_delta_std),min(avg_delta))
             ETA_max = (episodes-i)*(avg_delta_mean+avg_delta_std)
 
+            avg_steps.append(steps) #unused for now
             per_step = t_delta/steps
+            steps_per_score = score/steps
 
             print(f"{current_time} \n"
-            f'Episode: **{i}**/{episodes}, Score: {score:.0f} ({score-last_score:.1f}), Steps: {steps}\n'
-            f'Average score: {avg_score:.0f} ({avg_score-last_avg_score:.1f})\n'
+            f'Episode: **{i}**/{episodes}, Score: {score:.0f} (Δ{score-last_score:5.1f}) \n'
+            f'Average score: {avg_score:.0f} (Δ{avg_score-last_avg_score:5.1f})\n'
             f'Episode time: {t_delta:.1f}s, average: {avg_delta_mean:.1f}s (±{avg_delta_std:.2f}),', 
-            f'ETA {ETA_avg/60:.0f}m ({ETA_min/60:.0f}m-{ETA_max/60:.0f}m) \n'
-            f'{per_step:.1e}s per step \n')
+            f'ETA: {ETA_avg/60:.0f}m ({ETA_min/60:.0f}m-{ETA_max/60:.0f}m) \n'
+            f'Steps: {steps}. Time per step: {per_step:.1e}s. Reward per step: {steps_per_score:.2f}. \n' 
+            f'It has been {i - last_save} episode(s) since the model was last saved, with a score of {best_score:.0f} (Δ{avg_score-best_score:.0f}). \n')
 
             last_score = score
             last_avg_score = avg_score
@@ -132,4 +133,4 @@ if __name__ == '__main__':
 t2 = time.localtime()
 current_time = time.strftime("%Y-%m-%d-%H:%M:%S", t2)
 t_delta = time.mktime(t2)-time.mktime(t_start)
-print(f"----------------- Training ended at {current_time}. Duration was {t_delta/60:.2f} minutes.-----------------")
+print(f"\n----------------- Training ended at {current_time}. Duration was {t_delta/60:.2f} minutes.-----------------")
--- Walker with comments/render.py
+++ Walker with comments/render.py
@@ -14,7 +14,7 @@ agent = Agent(input_dims=env.observation_space.shape, env=env,
         n_actions=env.action_space.shape[0])
 
 #timesteps that are rendered
-timesteps = 3000
+timesteps = 300
 
 
 #load the model
@@ -32,12 +32,19 @@ agent.load_models()
 
 
 #renders the environment with the actions of the saved model
+counter = 0
+score = 0
 for _ in range(timesteps):
     env.render()
     #agent chooses action without noise
     action = agent.choose_action(observation, evaluate = True)
     observation, reward, done, info = env.step(action)
+    counter += 1
+    score += reward
 
     if done:
         observation = env.reset()
+        print(f"{counter} steps taken. Score: {score}")
+print(f"{counter} steps taken. Score: {score}")
+
 env.close()
